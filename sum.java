41bd2a10a4d3bfee42417182b68fffdbf4ec1ee9
shailu-506 <jntuhshailu@gmail.com>
2024-12-12 16:05:24 +0530

    adding two num

---
+++ sum.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        // Create a Scanner object to take input from the user
+        Scanner scanner = new Scanner(System.in);
+        
+        // Prompt the user to enter the first number
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        
+        // Prompt the user to enter the second number
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        
+        // Add the two numbers
+        double sum = num1 + num2;
+        
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        
+        // Close the scanner object to prevent resource leaks
+        scanner.close();
+    }
+}
+
